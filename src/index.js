08025ee21a4fe386fc1812b7886dfda8d3e7cbd6
taverasmisael <macrot29@gmail.com>
2018-01-16 02:11:22 -0400

    refact(root): split styles and index in src functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WebFont from 'webfontloader'
-
-import registerServiceWorker from './utils/registerServiceWorker';
-
-import 'minireset.css'
 
 import Routes from './routes'
 
+import registerServiceWorker from './utils/registerServiceWorker';
+
+import './styles'
+
 ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
-
-WebFont.load({
-  google: {
-    families: ['Roboto:300,400,500', 'sans-serif']
-  }
-})
